9560ea1ba6b75c3910dceeea4daf1f5a4537efd9
alejo <alejo.daraio@melon.tech>
2020-01-14 17:27:38 -0300

    server config

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3333;
 // This serves static files from the specified directory
 app.use(express.static(__dirname));
 
+//server
 const server = app.listen(PORT, () => {
   console.log('Server online...');
 });
