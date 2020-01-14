8f3d9c72753e9ed0cc5def1df4bd3b97bf5c2540
alejo <alejo.daraio@melon.tech>
2020-01-14 17:13:20 -0300

    make it better

---
+++ js/chuck-messages.js
@@ -0,0 +1,32 @@
+const ChuckMessage = async () => {
+
+  try {
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
+    const message = await response.json();
+    return message.value;
+  }
+  catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const renderChuckMessage = () => {
+  ChuckMessage().then(message => {
+    document.querySelector('#random-message').innerHTML = message;
+  });
+};
+
+const renderChuckOfflineMessage = () => {
+  document.querySelector('#random-message').innerHTML = 'Chuck is offline, sry.';
+};
+
+window.addEventListener('offline', event => {
+  renderChuckOfflineMessage();
+});
+
+window.addEventListener('online', event => {
+  renderChuckMessage();
+});
+
+// init first message.
+renderChuckMessage();
---
+++ sw.js
@@ -0,0 +1,64 @@
+const cacheName = 'cache-version-1';
+const precache = [
+  '/',
+  'js/main.js',
+  'css/main.css',
+  'index.html',
+  'pages/offline.html',
+  'pages/404.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+      caches.open(cacheName).then(cache => {
+        console.log('SW installed, precache configured: ', precache);
+        return cache.addAll(precache)
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+
+  const cacheWhitelist = [cacheName];
+
+  // Remove outdate cache files.
+  event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+            cacheNames.map(cacheName => {
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+            })
+        )
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Check if exists on cache to render else it will cache the new request.
+  event.respondWith(
+      caches.open(cacheName).then(cache => {
+        return cache.match(event.request).then(response => {
+
+          return response || fetch(event.request).then(response => {
+
+            if (response.status === 404) {
+              return caches.match('pages/404.html');
+            }
+
+            // here we are creating a new cache file.
+            if (event.request.url !== 'https://api.chucknorris.io/jokes/random') {
+              cache.put(event.request, response.clone());
+            }
+
+            return response;
+          })
+        }).catch(error => {
+          // if the request fails ( offline mode )
+          // we will show a custom offline page.
+          return caches.match('pages/offline.html');
+        });
+      })
+  );
+});
