ff1568bf2e526505b0941b6500816862ab74cd39
alejo <alejo.daraio@melon.tech>
2020-01-15 11:31:52 -0300

    server config 2\3

--- sw_install.js
+++ sw_install.js
@@ -8,32 +8,32 @@ if ('serviceWorker' in navigator) {
 else {
   console.log("Service worker not supported.")
 }
-
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', event => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  event.preventDefault();
-
-  // Stash the event so it can be triggered later.
-  deferredPrompt = event;
-
-  // Attach the install prompt to a user gesture
-  document.querySelector('#installBtn').addEventListener('click', event => {
-
-    // Show the prompt
-    deferredPrompt.prompt();
-
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            //  console.log('User accepted the A2HS prompt');
-            document.querySelector('#installBanner').style.display = 'none';
-          }
-          deferredPrompt = null;
-        });
-  });
-
-  // Update UI notify the user they can add to home screen
-  document.querySelector('#installBanner').style.display = 'flex';
-});
+//
+// let deferredPrompt;
+// window.addEventListener('beforeinstallprompt', event => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   event.preventDefault();
+//
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = event;
+//
+//   // Attach the install prompt to a user gesture
+//   document.querySelector('#installBtn').addEventListener('click', event => {
+//
+//     // Show the prompt
+//     deferredPrompt.prompt();
+//
+//     // Wait for the user to respond to the prompt
+//     deferredPrompt.userChoice
+//         .then((choiceResult) => {
+//           if (choiceResult.outcome === 'accepted') {
+//             //  console.log('User accepted the A2HS prompt');
+//             document.querySelector('#installBanner').style.display = 'none';
+//           }
+//           deferredPrompt = null;
+//         });
+//   });
+//
+//   // Update UI notify the user they can add to home screen
+//   document.querySelector('#installBanner').style.display = 'flex';
+// });
