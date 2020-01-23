ad0ecaa836755e050deff3a7085de33e8bb4b63b
alejo <alejo.daraio@melon.tech>
2020-01-23 12:49:24 -0300

    compress

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3333;
+const compression = require('compression');
 
 // This serves static files from the specified directory
 app.use(express.static(__dirname));
+// compress.
+app.use(compression());
 
 //server
 const server = app.listen(PORT, () => {
